cdb9b6a9f4bb2953911ba0f621202439e48456a3
knightss27 <knightss@catlin.edu>
2022-06-15 20:02:35 +0000

    fix: correct new-board source location (#14)

--- server/server.py
+++ server/server.py
@@ -50,10 +50,10 @@ def with_json(*outer_args):
     return decorator
 
 
-# /new-board gives us the compiled format of our source material aftre it has been run through Lexstat
+# /new-board gives us the compiled format of our source material after it has been run through Lexstat
 @app.route("/new-board")
 def new_board():
-    return compile_to_json("../output/burmish-pipeline/stage2/burmish-stage2-tmp-merged.tsv")
+    return compile_to_json("../pipeline/output/burmish-pipeline/stage2/burmish-stage2-tmp-merged.tsv")
 
 
 # /refish-board returns the output of the refishing algorithm for cognate reassignment 
